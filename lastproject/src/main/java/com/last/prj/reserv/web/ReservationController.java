79209a6d164562df84d9a1fc47b9109bcec05edb
백승국 <das01042@naver.com>
2022-02-21 21:10:20 +0900

    'calendar'

--- lastproject/src/main/java/com/last/prj/reserv/web/ReservationController.java
+++ lastproject/src/main/java/com/last/prj/reserv/web/ReservationController.java
@@ -2,11 +2,14 @@ package com.last.prj.reserv.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.last.prj.reserv.service.FullcalendarService;
 import com.last.prj.reserv.service.ReservationService;
 import com.last.prj.reserv.service.ReservationVO;
 
@@ -17,8 +20,12 @@ public class ReservationController {
 	@Autowired
 	private ReservationService reservationDao;
 	
+	@Autowired
+	private FullcalendarService fullcalendarDao;
+	
 	@RequestMapping("/reservationSelectList")
-	public String reservationSelectList(Model model) {
+	public String reservationSelectList(Model model,HttpSession session) {
+		
 		List<ReservationVO> list = reservationDao.reservationSelectList();
 		model.addAttribute("reservation", list);
 		System.out.println(list);
