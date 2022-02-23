6ffa3b0d1f84a4f8723cd82500624a393eec8fa1
bbmo <bbmo1626@gmail.com>
2022-02-23 17:41:12 +0900

    0223 b_branch

--- lastproject/src/main/java/com/last/prj/ffile/web/FfileController.java
+++ lastproject/src/main/java/com/last/prj/ffile/web/FfileController.java
@@ -1,7 +1,10 @@
 package com.last.prj.ffile.web;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletContext;
@@ -9,10 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.last.prj.ffile.service.FfileService;
@@ -27,35 +28,61 @@ public class FfileController {
 	ServletContext sc;
 	
 	@PostMapping("/multiFile")
-	public int multiFileUpload(@RequestParam("multiFile") List<MultipartFile> multiFileList, FfileVO ffile, HttpServletRequest request) {
-		System.out.println("multiFileList : " + multiFileList);
+	public String multiFileUpload(@RequestParam("multiFile") List<MultipartFile> multiFileList,@RequestParam int f_part,@RequestParam String w_write, FfileVO ffile, HttpServletRequest request) {
 		
-		String originalFileName = file.getOriginalFilename();
-
-		String webPath = "/resources/upload";
-		String realPath = sc.getRealPath(webPath);
-		System.out.printf("realPath: %s\n", realPath);
-
-		File savePath = new File(realPath);
-		if (!savePath.exists())
-			savePath.mkdirs();
-
-		realPath += File.separator + originalFileName;
-		File saveFile = new File(realPath);
-
-		if (!originalFileName.isEmpty()) {
-			String uuid = UUID.randomUUID().toString();
-			String saveFileName = uuid + originalFileName.substring(originalFileName.lastIndexOf("."));
-
-			try {
-				file.transferTo(saveFile);
-				ffile.setPicture(originalFileName);
-				ffile.setPfile(saveFileName);
-			} catch (Exception e) {
-				e.printStackTrace();
+			System.out.println("multiFileList : " + multiFileList);
+				
+			// path 가져오기
+			String webPath = "/resources/upload";
+			String realPath = sc.getRealPath(webPath);
+			
+			System.out.printf("realPath: %s\n", realPath);
+			
+			File fileCheck = new File(realPath);
+			
+			if(!fileCheck.exists()) fileCheck.mkdirs();
+				
+				
+			List<Map<String, String>> fileList = new ArrayList<Map<String, String>>();
+				
+			for(int i = 0; i < multiFileList.size(); i++) {
+				String originFile = multiFileList.get(i).getOriginalFilename();
+				String ext = originFile.substring(originFile.lastIndexOf("."));
+				String changeFile = UUID.randomUUID().toString() + ext;
+					
+					
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("originFile", originFile);
+				map.put("changeFile", changeFile);
+				
+				ffile.setF_part(f_part);
+				ffile.setW_write(w_write);
+				ffile.setPicture(originFile);
+				ffile.setPfile(changeFile);
+				
+				ffileDao.ffileInsert(ffile);
+				
+				fileList.add(map);
 			}
-		}
+				
+			// 파일업로드
+			try {
+				for(int i = 0; i < multiFileList.size(); i++) {
+					File uploadFile = new File(realPath + "\\" + fileList.get(i).get("changeFile"));
+					multiFileList.get(i).transferTo(uploadFile);
+				}
+					
+				System.out.println("다중 파일 업로드 성공");
+					
+				} catch (Exception e) {
+					System.out.println("다중 파일 업로드 실패");
+					// 만약 업로드 실패하면 파일 삭제
+					for(int i = 0; i < multiFileList.size(); i++) {
+						new File(realPath + "\\" + fileList.get(i).get("changeFile")).delete();
+					}
 
-		return ffileDao.ffileInsert(ffile);
+					e.printStackTrace();
+				}
+			return "mypage/memberMypage";		
+		}
 	}
-}
--- lastproject/src/main/webapp/WEB-INF/views/pmember/memberList.jsp
+++ lastproject/src/main/webapp/WEB-INF/views/pmember/memberList.jsp
@@ -38,14 +38,17 @@
 				</c:forEach>
 			</div>
 		</div>
-		<div class="input-group">
-			<input type="text" class="form-control"> 
-			<span class="input-group-btn">
-				<button class="btn btn-default" type="button">
-					<i class="fa fa-search"></i>
-				</button>
-			</span>
-		</div>
+		<div class="blog_right_sidebar">
+		<aside class="single_sidebar_widget search_widget">
+                            <div class="input-group">
+                                <input type="text" class="form-control">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                                </span>
+                            </div><!-- /input-group -->
+                            <div class="br"></div>
+                        </aside>
+                        </div>
 		<!-- /input-group -->
 	</section>
 </body>
