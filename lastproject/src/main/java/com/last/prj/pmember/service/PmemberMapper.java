15cf137de4e5a3863dcbdcd0d9418c7d126c4d21
lsyoung <sulyoung7@gmail.com>
2022-02-28 17:43:53 +0900

    228기념

--- lastproject/src/main/java/com/last/prj/pmember/service/PmemberMapper.java
+++ lastproject/src/main/java/com/last/prj/pmember/service/PmemberMapper.java
@@ -8,6 +8,8 @@ public interface PmemberMapper {
 	public List<ReviewVO> getCounselReview(String p_id); //상담리뷰
 	public List<ReviewVO> getServiceReview(String p_id); //서비스리뷰
 	public List<FfileVO> getProfile(String m_id);//회원프로필사진
+	public int memberPage(Criteria cri);//페이징 총 회원수
+	public List<PmemberVO> memberPageList(Criteria cri);//페이징 리스트
 
 	//관리자
 	public List<PmemberVO> admPlistCode(int code);
@@ -16,8 +18,7 @@ public interface PmemberMapper {
 	//id로 정보 조회
 	public PmemberVO PmemberOne(String p_id);
 	
-	public int memberPage(Criteria cri);
-	public List<PmemberVO> memberPageList(Criteria cri);
+	
 
 }
 
