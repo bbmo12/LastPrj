fb7b2b387a41a358fa3f9eded77348432cdcb6a0
bbmo12 <bbmo1626@gmail.com>
2022-03-02 16:57:42 +0900

    0302 16:57 commit

--- lastproject/src/main/java/com/last/prj/notice/service/NoticeService.java
+++ lastproject/src/main/java/com/last/prj/notice/service/NoticeService.java
@@ -1,5 +1,8 @@
 package com.last.prj.notice.service;
 
+import java.util.List;
+
 public interface NoticeService {
 	int noticeInsert(NoticeVO notice);
+	List<NoticeVO> noticeSelectList(String m_id);
 }
