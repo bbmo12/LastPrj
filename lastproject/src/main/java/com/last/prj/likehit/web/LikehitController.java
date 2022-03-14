60ac5e72165398c7f8a3e966266c936018db7d9f
lsyoung <sulyoung7@gmail.com>
2022-03-14 16:31:44 +0900

    3/14

--- lastproject/src/main/java/com/last/prj/likehit/web/LikehitController.java
+++ lastproject/src/main/java/com/last/prj/likehit/web/LikehitController.java
@@ -1,9 +1,9 @@
 package com.last.prj.likehit.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.last.prj.likehit.service.LikehitService;
 import com.last.prj.likehit.service.LikehitVO;
 import com.last.prj.pmember.service.PmemberService;
+import com.last.prj.security.CustomUser;
 
 @Controller
 public class LikehitController {
@@ -23,9 +24,14 @@ public class LikehitController {
 	
 	@RequestMapping("pmemberLike")//추천수
 	@ResponseBody
-	public int pmemberLike(@RequestParam("p_id")String p_id, LikehitVO hit, HttpServletRequest request) {
-		HttpSession session = request.getSession();
-		String m_id = (String) session.getAttribute("mId");
+	public int pmemberLike(@RequestParam("p_id")String p_id, LikehitVO hit, Principal principal) {
+		String m_id = "0";
+		if(principal != null) {
+			CustomUser userDetails = (CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			if(userDetails.getRole() == "일반회원") {
+				m_id  =userDetails.getMember().getM_id();
+			}
+		}
 		hit.setM_id(m_id);
 		
 		int likeCheck = likehitDao.likeCheck(hit);
